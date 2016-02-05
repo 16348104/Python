3972a5f3cfd93d1585c55be44edd7910cfaf26ec
16348104 <16348104@QQ.com>
2016-02-05 21:24:35 +0800

    update python

--- 3-1/3_1.py
+++ 3-1/3_1.py
@@ -1,9 +1,8 @@
-# print 'hello,world'
-# print '300+100=', 300 + 100
+
 list = ['Adam', 95.5, 'Lisa', 85, 'Bar', 59]
 list.append('Pual')
 list.insert(0, 'pual')
-print list
+# print list
 # print list[0]
 # print list[2]
 # print list[-1]
@@ -11,5 +10,5 @@ print list
 # print list[-5]
 list2 = ['Adam', 'Lisa', 'Bar']
 list2.insert(2, 'Pual')
-for name in list2:
-    print list2
+# for name in list2:
+# print list2
