6b01000cfdfbbb581f2e5a98732c187150affc36
16348104 <16348104@QQ.com>
2016-02-15 12:32:39 +0800

    update python5-1

---
+++ 3-6/3-6.py
@@ -0,0 +1,9 @@
+print '''
+line1
+line2
+line3'''
+print r'\(~_~)/\(~_~)/'
+str = r'''Python was started in 1989 by "Guido".
+It is free and easy to learn,
+Let's start learn Python in imooc.'''
+print str
