48d21671d646ba6e7456d33265c2cd952cd9d114
16348104 <16348104@qq.com>
2016-10-31 16:57:37 +0800

    4week

--- icoures/4week/quadratic.py
+++ icoures/4week/quadratic.py
@@ -1,2 +1,17 @@
-__author__ = 'zb'
+import math
+
+def main():
+    print("The program find the real solutions to quadratic\n ")
+    a, b, c = eval(input("Please enter the coefficients (a,b,c):"))
+    delta = b * b - 4 * a * c
+    if delta < 0:
+        print("\nThe equation has no real roots ")
+    else:
+        delta = math.sqrt(delta)
+        r1 = (-b + delta) / (2 * a)
+        r2 = (-b - delta) / (2 * a)
+        print("\nThe solutions are:", r1, r2)
+main()
+
+
 
