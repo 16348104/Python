55812d7df7ed3960b3239a200b2a7a733eeb0e56
16348104 <16348104@qq.com>
2017-02-03 20:51:02 +0800

    updata

---
+++ 哈工大/鸡兔同笼.py
@@ -0,0 +1,13 @@
+for c in range(36):
+    for r in range(36):
+        if c + r == 35 and c * 2 + r * 4 == 94:
+            print 'The number of chickens is:', c
+            print 'The number of rabbits is:', r
+            print
+
+
+
+
+
+# 大约在1500年前。大约在1500年前,《孙子算经》中就记载了这个有趣的问题。
+# 书中是这样叙述的:“今有雉兔同笼，上有三十五头,下有九十四足,问雉兔各几何?”
