66252691944ac0d49a2517054c5f719e4469a118
16348104 <16348104@qq.com>
2017-02-15 11:42:05 +0800

    test

--- 哈工大/test.py
+++ 哈工大/test.py
@@ -1,5 +1,12 @@
 # __author__ = 'zb'
-def defaultParameters(arg1, arg2=2, arg3=3):
-    print arg1, arg2, arg3
+# def defaultParameters(arg1, arg2=2, arg3=3):
+# print arg1, arg2, arg3
+#
+# defaultParameters(10)
 
-defaultParameters(10)
+
+# number = 30
+# if number % 2 == 0:
+#     print number, 'is even'
+# elif number % 3 == 0:
+#     print number, 'is multiple of 3'
