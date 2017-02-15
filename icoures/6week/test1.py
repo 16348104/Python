32fdb8cc3d96c22304f2f6608db16e23c66f505b
16348104 <16348104@qq.com>
2017-02-15 16:40:06 +0800

    阶乘

--- icoures/6week/test1.py
+++ icoures/6week/test1.py
@@ -1,6 +1,6 @@
 # __author__ = 'zb'
 def main():
-    fname = eval(input("Enter filename:"))
+    fname = eval(input("输入文件名:"))
     infile = open(fname, "r")
     data = infile.read()
     print(data)
---
+++ 哈工大/！.py
@@ -0,0 +1,14 @@
+# __author__ = 'zb'
+'''
+def p(n):
+    x = 1
+    i = 1
+    while i <= n:
+        x *= i
+        i += 1
+    return x
+
+
+n = int(raw_input("input:"))
+print n, "p(!):", p(n)
+'''
