ded58f19ddb01350f6f7d3af815868419bceb697
16348104 <16348104@qq.com>
2017-02-19 01:41:24 +0800

    update names

--- 哈工大/5/names.py
+++ 哈工大/5/names.py
@@ -1,7 +1,23 @@
 # __author__ = 'dell'
 
 f = open('names.txt', 'r')
+
+
+def is_panlindrom(name):
+    low = 0
+    hight = len(name) - 1
+    while low < hight:
+        if name[low] != name[hight]:
+            return False
+
+        low += 1
+        hight -= 1
+
+    return True
+
+
 for line in f:
     line = line.strip()
-    print line.title()
+    if is_panlindrom(line):
+        print line
 f.close()
