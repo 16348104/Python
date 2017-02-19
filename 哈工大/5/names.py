7c8ca6213091a4515ee128368c94c77c987a555b
16348104 <16348104@qq.com>
2017-02-19 12:56:12 +0800

    update names

--- 哈工大/5/names.py
+++ 哈工大/5/names.py
@@ -1,4 +1,5 @@
 # __author__ = 'dell'
+import re
 
 f = open('names.txt', 'r')
 
@@ -18,15 +19,28 @@ def is_panlindrom(name):
 def is_panlindrom_rec(name):
     if len(name) < 2:
         return True
-    if name[0]==name[-1]:
+    if name[0] == name[-1]:
         return is_panlindrom_rec(name[1:-1])
     else:
         return False
 
 
+def is_ascending(name):
+    p = name[0]
+    for c in name:
+        if p < c:
+            return False
+        p == c
+    return True
+
+# regular expressions
 for line in f:
+    pattern = 'C.A'
+    result = re.search(pattern, line)
     line = line.strip()
-    if is_panlindrom_rec(line):
-        # if is_panlindrom(L):
-        print line
+    # if is_panlindrom_rec(line):
+    # if is_panlindrom(L):
+    # if is_ascending(line):
+    if result:
+        print 'name is {}'.format(line)
 f.close()
