f1613be44dbb78d14966bb669e760ea3e6dcd650
16348104 <16348104@qq.com>
2017-02-25 19:08:26 +0800

    update search

--- 哈工大/列表/search.py
+++ 哈工大/列表/search.py
@@ -2,9 +2,14 @@
 def search(lst, x):
     for i in range(len(lst)):
         if lst[i] == x:
-            return lst[i]
+            return i
 
     return -1
+
+
 lst = [10, 6, 7, 8]
 print search(lst, 10)
-print search(lst, 1)
+# print search(lst, 1)
+print lst.index(8)
+
+print [1, 2, 2].index(2)
