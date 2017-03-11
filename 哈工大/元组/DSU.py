be337ee70175bc24313343f66726fb84bdbbde63
16348104 <16348104@QQ.com>
2017-03-11 18:52:49 +0800

    update dsu

--- 哈工大/元组/DSU.py
+++ 哈工大/元组/DSU.py
@@ -1,14 +1,15 @@
-words = ['dfafj', 'ajgjg', 'ghgha','tu']
-#decorate
-lst = []
-for w in words:
-    lst.append((len(w), w))
-#sort
-lst.sort(reverse=True)
-res = []
-#undecorate
-for length, word in lst:
-        res.append(word)
-
-print res
-
+words = ['dfafj', 'ajgjg', 'ghghaf', 'tu']
+# #decorate
+# lst = []
+# for w in words:
+#     lst.append((len(w), w))
+# #sort
+# lst.sort(reverse=True)
+# res = []
+# #undecorate
+# for length, word in lst:
+#         res.append(word)
+#
+# print res
+words.sort(key=lambda x: len(x), reverse=True)
+print words
