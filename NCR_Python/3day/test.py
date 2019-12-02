6c344fcaf3cfa7ff647dd410dc1030e2fca81f31
16348104 <16348104@qq.com>
2019-12-02 17:40:18 +0800

    test  vscode

---
+++ NCR_Python/3day/test.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+def people(age):
+    if age > 0:
+        print("正常")
+        name = get_name()
+    else:
+        print('不正常!')
+
+
+def get_name():
+    return 'abc'
+
+
+people(10)
